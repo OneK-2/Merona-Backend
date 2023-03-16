17e95b4eacd2a101e08ff4a994c0d27beb0fa728
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-03-16 13:15:25 +0900

    initial commit

---
+++ src/main/java/merona/nabdbackend/NabdBackendApplication.java
@@ -0,0 +1,13 @@
+package merona.nabdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabdBackendApplication.class, args);
+	}
+
+}
