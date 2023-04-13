f45b128a7f7fa351d25c3c4e11e428b6721ec040
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-04-13 19:45:31 +0900

    feat: 로그인, 토큰 재발행 기능 추가

--- src/main/java/merona/nabdbackend/user/controller/UserController.java
+++ src/main/java/merona/nabdbackend/user/controller/UserController.java
@@ -2,6 +2,9 @@ package merona.nabdbackend.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import merona.nabdbackend.auth.TokenDto;
+import merona.nabdbackend.auth.TokenRequestDto;
+import merona.nabdbackend.user.dto.LoginRequestDto;
 import merona.nabdbackend.user.dto.SignUpRequestDto;
 import merona.nabdbackend.user.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -22,5 +25,16 @@ public class UserController {
         log.info("TEST");
         String email = userService.signUp(requestDto);
         return ResponseEntity.ok().body(email);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody LoginRequestDto dto) {
+        TokenDto tokenDto = userService.login(dto);
+        return ResponseEntity.ok().body(tokenDto);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(userService.reissue(tokenRequestDto));
     }
 }
--- src/main/java/merona/nabdbackend/user/service/UserService.java
+++ src/main/java/merona/nabdbackend/user/service/UserService.java
@@ -1,9 +1,16 @@
 package merona.nabdbackend.user.service;
 
 import lombok.RequiredArgsConstructor;
+import merona.nabdbackend.auth.*;
+import merona.nabdbackend.auth.token.RefreshToken;
+import merona.nabdbackend.auth.token.RefreshTokenRepository;
+import merona.nabdbackend.user.dto.LoginRequestDto;
 import merona.nabdbackend.user.dto.SignUpRequestDto;
 import merona.nabdbackend.user.entity.User;
 import merona.nabdbackend.user.repository.UserRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +21,13 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Transactional
 public class UserService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    //private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public String signUp(SignUpRequestDto requestDto) {
         Optional<User> userByEmail = userRepository.findUserByEmail(requestDto.getEmail());
         if (userByEmail.isPresent()) {
@@ -28,6 +40,56 @@ public class UserService {
         userRepository.save(user);
 
         return user.getEmail();
+    }
+
+    @Transactional
+    public TokenDto login(LoginRequestDto dto) {
+        // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
+
+        // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
+        //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 생성
+        TokenDto tokenDto = jwtProvider.generateToken(authentication);
+
+        // 4. RefreshToken 저장
+        RefreshToken refreshToken = RefreshToken.builder()
+                .key(authentication.getName())
+                .value(tokenDto.getRefreshToken())
+                .build();
+        refreshTokenRepository.save(refreshToken);
+
+        return tokenDto;
+    }
+
+    @Transactional
+    public TokenDto reissue(TokenRequestDto tokenRequestDto){
+        // 1. Refresh Token 검증
+        if (!jwtProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+        }
+        // 2. Access Token 에서 Member ID 가져오기
+        Authentication authentication = jwtProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+        }
+
+        // 5. 새로운 토큰 생성
+        TokenDto tokenDto = jwtProvider.generateToken(authentication);
+
+        // 6. 저장소 정보 업데이트
+        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+        refreshTokenRepository.save(newRefreshToken);
+
+        return tokenDto;
     }
 
 }
