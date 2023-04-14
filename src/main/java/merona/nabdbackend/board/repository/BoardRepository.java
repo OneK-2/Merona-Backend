6952680115e36f9487a2406b5f2ed4610a8de2cf
minjeongkk <kunvx@naver.com>
2023-04-14 19:37:49 +0900

    feat : 게시글 작성 기능 구현

---
+++ src/main/java/merona/nabdbackend/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package merona.nabdbackend.board.repository;
+
+import merona.nabdbackend.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
