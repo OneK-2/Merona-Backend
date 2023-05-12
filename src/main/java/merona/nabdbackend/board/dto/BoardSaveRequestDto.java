69e5e9efd3bce73ae1f76e4f27e6686eb79a884a
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-05-12 21:46:10 +0900

    feat: 게시글 cost 속성 추가 및 기능 수정

--- src/main/java/merona/nabdbackend/board/dto/BoardSaveRequestDto.java
+++ src/main/java/merona/nabdbackend/board/dto/BoardSaveRequestDto.java
@@ -7,6 +7,8 @@ import merona.nabdbackend.board.entity.Address;
 import merona.nabdbackend.board.entity.Board;
 import merona.nabdbackend.user.entity.User;
 
+import javax.validation.constraints.Positive;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,16 +16,18 @@ public class BoardSaveRequestDto {
     private User user;
     private String title;
     private String contents;
+    @Positive
+    private Integer cost;
     private String zipcode;
     private String streetAddress;
     private String detailAddress;
     // 주소
 
     public Board boardFormDto() {
-        return new Board(user, title, contents, new Address(zipcode,streetAddress,detailAddress));
+        return new Board(user, title, contents, cost, new Address(zipcode, streetAddress, detailAddress));
     }
 
-    public void setUser(User user){
+    public void setUser(User user) {
         this.user = user;
     }
 
--- src/main/java/merona/nabdbackend/board/dto/BoardUpdateRequestDto.java
+++ src/main/java/merona/nabdbackend/board/dto/BoardUpdateRequestDto.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Positive;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardUpdateRequestDto {
     private String title;
     private String contents;
+    @Positive
+    private Integer cost;
     private String zipcode;
     private String streetAddress;
     private String detailAddress;
